864214da26684fc34ddd458365e40539eeb884bb
gaurav <dashore.gourav@thinksys.com>
2021-04-15 12:27:42 +0530

    Update EvenSquareSum.js

--- function scoping/EvenSquareSum.js
+++ function scoping/EvenSquareSum.js
@@ -1,14 +1,19 @@
-let num=[1,2,3,4,5,6,7,8,9,10];
-var b=num.filter(function(items,index){
-    return index%2==0;
-})
-console.log("Even Place Elements:",b);
-var c=b.map(function(items,index){
-    return items*items;
-})
-console.log("Even Place Elements Square=",c);
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const EvenNumbers = num.filter(getEvenNumbers);
 
-var d=c.reduce(function(sum,item){
-    return sum+=item; 
-})
-console.log("Final Result=",d);
+function getEvenNumbers(items) {
+    return items % 2 == 0;
+};
+
+console.log("Even Elements:", EvenNumbers);
+const EvenNumberSquares = EvenNumbers.map(getSquare);
+function getSquare(items) {
+    return items * items;
+};
+
+console.log("Even Elements Squares=", EvenNumberSquares);
+const result = EvenNumberSquares.reduce(getSum, 0);
+function getSum(sum, items) {
+    return sum += items;
+};
+console.log("Final Result=", result);
